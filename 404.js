2c05533ef8921a8047f655d816da2847bf7ac72a
Zeno Zeng <zenoes@qq.com>
2013-11-17 20:06:58 +0800

    update view for 404

--- 404.js
+++ 404.js
@@ -6,12 +6,11 @@ var data = {
     }
 };
 var href = window.location.href;
-var key = href.replace(new RegExp('http:\/\/[^\/]*\/'), '');
-if(typeof data.key != "undefined") {
-    var item = data.key;
+var key = href.split('/').pop();
+if(typeof data[key] != "undefined") {
+    var item = data[key];
     var html = '<iframe src="';
     html += item.url;
     html += '" frameborder="0" scrolling="auto" width="100%" height="100%"></iframe>';
-    html += '<div id="info">'+item.name+' - '+item.description+'</div>';
     document.body.innerHTML = html;
 }
