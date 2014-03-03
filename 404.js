0717eeb9cdd9199e685e6a642a06948f7be14efd
Zeno Zeng <zenoofzeng@gmail.com>
2014-03-03 16:31:23 +0800

    update 404.js

--- 404.js
+++ 404.js
@@ -2,7 +2,7 @@ var data = {
     idi: {
         "name": "International Design Institute (IDI) ZHEJIANG UNIVERSITY",
         "url": "http://zenozeng.github.io/www.idi.zju.edu.cn",
-        "description": "www.idi.zju.edu.cn"
+        "description": "www.idi.zju.edu.cn/"
     },
     vs: {
         "name": "VS",
@@ -11,11 +11,14 @@ var data = {
     }
 };
 var href = window.location.href;
-var key = href.split('/').pop();
+var paths = href.split('demo.zenozeng.com/').pop().split('/');
+var key = paths.shift();
+var path = paths.join('/');
 if(typeof data[key] != "undefined") {
     var item = data[key];
     var html = '<iframe src="';
     html += item.url;
+    html += path;
     html += '" frameborder="0" scrolling="auto" width="100%" height="100%"></iframe>';
     document.body.innerHTML = html;
 }
